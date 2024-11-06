1b22b871bb7431115c32d4b3023f15d67053e599
asma832 <asma.frikha@esprit.tn>
2024-11-06 23:45:28 +0100

    connexion

--- src/main/java/tn/esprit/devops_project/entities/StockDTO.java
+++ src/main/java/tn/esprit/devops_project/entities/StockDTO.java
@@ -3,17 +3,21 @@ package tn.esprit.devops_project.entities;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Set;
 
+@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class StockDTO implements Serializable {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     long idStock;
     String title;
-    private Set<ProductDTO> products; // Ajoutez un DTO pour le produit si ce n'est pas encore fait
+    @OneToMany(mappedBy = "stock")
+    private Set<ProductDTO> products;
 }
